0a4c588dd525a2a0abd95cd5302a264e9d228fb5
Tuğsan Yeşil <tugsanyesil@gmail.com>
2023-08-05 03:33:12 +0300

    Add project files.

---
+++ LogisticMap/LogisticMapForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace LogisticMap
+{
+    partial class LogisticMapForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.LogisticMapChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.LogRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.X0TrackBar = new System.Windows.Forms.TrackBar();
+            this.RTrackBar = new System.Windows.Forms.TrackBar();
+            this.X0Label = new System.Windows.Forms.Label();
+            this.RLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LogisticMapChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X0TrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LogisticMapChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.LogisticMapChart.ChartAreas.Add(chartArea2);
+            this.LogisticMapChart.Location = new System.Drawing.Point(12, 12);
+            this.LogisticMapChart.Name = "LogisticMapChart";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Name = "Series1";
+            this.LogisticMapChart.Series.Add(series2);
+            this.LogisticMapChart.Size = new System.Drawing.Size(420, 300);
+            this.LogisticMapChart.TabIndex = 0;
+            // 
+            // LogRichTextBox
+            // 
+            this.LogRichTextBox.Location = new System.Drawing.Point(15, 420);
+            this.LogRichTextBox.Name = "LogRichTextBox";
+            this.LogRichTextBox.Size = new System.Drawing.Size(417, 162);
+            this.LogRichTextBox.TabIndex = 1;
+            this.LogRichTextBox.Text = "";
+            // 
+            // X0TrackBar
+            // 
+            this.X0TrackBar.Location = new System.Drawing.Point(37, 318);
+            this.X0TrackBar.Name = "X0TrackBar";
+            this.X0TrackBar.Size = new System.Drawing.Size(395, 45);
+            this.X0TrackBar.TabIndex = 3;
+            this.X0TrackBar.Scroll += new System.EventHandler(this.X0TrackBar_Scroll);
+            // 
+            // RTrackBar
+            // 
+            this.RTrackBar.Location = new System.Drawing.Point(37, 369);
+            this.RTrackBar.Name = "RTrackBar";
+            this.RTrackBar.Size = new System.Drawing.Size(395, 45);
+            this.RTrackBar.TabIndex = 4;
+            this.RTrackBar.Scroll += new System.EventHandler(this.RTrackBar_Scroll);
+            // 
+            // X0Label
+            // 
+            this.X0Label.AutoSize = true;
+            this.X0Label.Location = new System.Drawing.Point(12, 321);
+            this.X0Label.Name = "X0Label";
+            this.X0Label.Size = new System.Drawing.Size(26, 13);
+            this.X0Label.TabIndex = 5;
+            this.X0Label.Text = "X0 :";
+            // 
+            // RLabel
+            // 
+            this.RLabel.AutoSize = true;
+            this.RLabel.Location = new System.Drawing.Point(12, 369);
+            this.RLabel.Name = "RLabel";
+            this.RLabel.Size = new System.Drawing.Size(21, 13);
+            this.RLabel.TabIndex = 6;
+            this.RLabel.Text = "R :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 589);
+            this.Controls.Add(this.RLabel);
+            this.Controls.Add(this.X0Label);
+            this.Controls.Add(this.RTrackBar);
+            this.Controls.Add(this.X0TrackBar);
+            this.Controls.Add(this.LogRichTextBox);
+            this.Controls.Add(this.LogisticMapChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.LogisticMapChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X0TrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart LogisticMapChart;
+        private System.Windows.Forms.RichTextBox LogRichTextBox;
+        private System.Windows.Forms.TrackBar X0TrackBar;
+        private System.Windows.Forms.TrackBar RTrackBar;
+        private System.Windows.Forms.Label X0Label;
+        private System.Windows.Forms.Label RLabel;
+    }
+}
+
---
+++ LogisticMap/LogisticMapForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LogisticMap
+{
+    public partial class LogisticMapForm : Form
+    {
+        float x0 = 0.5f;
+        public float X0
+        {
+            get => x0;
+            set
+            {
+                x0 = value;
+                RTrackBar.Maximum = (x0 == 0.0) ? (int)(RTrackBarRatio * 100) : (int)(RTrackBarRatio * (1.0 / (x0 * (1.0 - x0))));
+                ChartLogisticMap();
+            }
+        }
+
+        public float R => RTrackBar.Value / RTrackBarRatio;
+        public float RTrackBarRatio = 100.0f;
+        public int XCount = 30;
+
+        public LogisticMapForm()
+        {
+            InitializeComponent();
+
+            X0TrackBar.Minimum = 0;
+            X0TrackBar.Maximum = 1000;
+
+            RTrackBar.Minimum = 0;
+            X0TrackBar.Value = (int)(X0TrackBar.Maximum * x0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X0 = x0;
+        }
+
+        private List<float> LogisticMap(float R, float X0)
+        {
+            List<float> Xs = new List<float>() { X0 };
+            for (int i = 0; i < XCount; i++)
+            {
+                X0 = R * X0 * (1 - X0);
+                if (X0 > 1 || X0 == 0) { break; }
+                Xs.Add(X0);
+            }
+            return Xs;
+        }
+
+        private void ChartLogisticMap()
+        {
+            var Xs = LogisticMap(R, X0);
+            LogisticMapChart.Series[0].Points.Clear();
+            LogRichTextBox.Text = $"X0 : {X0} R: {R} R_max : {RTrackBar.Maximum / RTrackBarRatio}\n";
+            for (int i = 0; i < Xs.Count; i++)
+            {
+                LogisticMapChart.Series[0].Points.AddXY(i, Xs[i]);
+                LogRichTextBox.Text += $" i : {i}  Xs[{i}] : {Xs[i]}\n";
+            }
+        }
+
+        private void X0TrackBar_Scroll(object sender, EventArgs e)
+        {
+            X0 = (float)X0TrackBar.Value / X0TrackBar.Maximum;
+        }
+
+        private void RTrackBar_Scroll(object sender, EventArgs e)
+        {
+            ChartLogisticMap();
+        }
+    }
+}
